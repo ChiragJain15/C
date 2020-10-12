6ee6e5171ee82aa6e1c9a63d885b21495bd2596a
chirag <chiragjain150302@gmail.com>
2020-10-12 08:42:04 +0530

    copying a file

--- FileCopy.c
+++ FileCopy.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    FILE fo,fc;
-    fo = fopen("studentRecord.txt",r);
+    FILE *fo,*fc;
+    fo = fopen("studentRecord.txt","r");
+    fc = fopen("studentRecordCpy.txt","w");
+    char ch;
+    while(1)
+    {
+        ch = fgetc(fo);
+        if (ch == EOF)
+            break;
+        fputc(ch, fc);
+    }
+    fclose(fo);
+    fclose(fc);
 }
